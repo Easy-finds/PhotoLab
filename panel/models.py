59238c207515c51560d50f4b2ec00700ad1406ba
NitinBargaje <nitinbargaje7@gmail.com>
2021-01-10 21:47:54 +0530

    first

--- panel/models.py
+++ panel/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+import datetime
 
-# Create your models here.
+
+class Customer(models.Model):
+    first_name = models.CharField(max_length=20)
+    last_name = models.CharField(max_length=20)
+    mobile = models.CharField(max_length=15)
+    email = models.EmailField()
+    password = models.CharField(max_length=500)
+
+    @staticmethod
+    def checkUser(email):
+        try:
+            return Customer.objects.get(email=email)
+        except:
+            return False
+
+    def isExists(self):
+        if Customer.objects.filter(email=self.email):
+            return True
+        else:
+            return False
+
+    def register(self):
+        self.save()
+
+    @staticmethod
+    def get_customer_by_id(id):
+        return Customer.objects.filter(id=id)
--- panel/views.py
+++ panel/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect, HttpResponseRedirect
+from django.http import HttpResponse
+from django.contrib.auth.hashers import make_password, check_password
+from .models import Customer
+from django.views import View
 # Create your views here.
+
+
+class Home(View):
+    def get(self, request):
+        return render(request, 'signup.html')
