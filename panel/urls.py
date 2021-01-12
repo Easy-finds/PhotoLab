e05e40451dd77138f691c9bc6506f89d797426e4
NitinBargaje <nitinbargaje7@gmail.com>
2021-01-12 16:17:40 +0530

    nothing

--- panel/urls.py
+++ panel/urls.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from django.urls import path
-from .views import Home
+from .views import Home, Signup
 
 urlpatterns = [
-    path('', Home.as_view(), name="home")
+    path('', Home.as_view(), name="home"),
+    path('signup', Signup.as_view(), name="signup")
 ]
--- panel/views.py
+++ panel/views.py
@@ -8,4 +8,9 @@ from django.views import View
 
 class Home(View):
     def get(self, request):
+        return render(request, 'login.html')
+
+
+class Signup(View):
+    def get(self, request):
         return render(request, 'signup.html')
